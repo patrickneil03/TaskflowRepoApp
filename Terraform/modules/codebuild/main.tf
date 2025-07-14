1077d0dadca2866f1dcb304ff9d093bde3225aba
PatrickBaylen <patrickbaylen3@gmail.com>
2025-07-14 19:13:20 +0800

    testing

--- Terraform/modules/codebuild/main.tf
+++ Terraform/modules/codebuild/main.tf
@@ -2,43 +2,37 @@ resource "aws_codebuild_project" "frontend_sync" {
   name          = "taskflow-frontend-sync"
   service_role  = var.cb_role_arn
 
-  artifacts {
-    type = "CODEPIPELINE"
+  artifacts { 
+    type = "CODEPIPELINE" 
   }
 
   environment {
-    compute_type    = "BUILD_GENERAL1_SMALL"
-    image           = "aws/codebuild/standard:6.0"
-    type            = "LINUX_CONTAINER"
-    privileged_mode = false
+    compute_type = "BUILD_GENERAL1_SMALL"
+    image        = "aws/codebuild/standard:6.0"
+    type         = "LINUX_CONTAINER"
 
     environment_variable {
       name  = "TARGET_BUCKET"
-      value = trim(var.s3_bucket_my_bucket, "/")
+      value = var.s3_bucket_my_bucket
     }
   }
 
   source {
     type      = "CODEPIPELINE"
-    buildspec = trimspace(<<BUILD_SPEC
+    buildspec = <<BUILD_SPEC
 version: 0.2
 
 phases:
   build:
     commands:
       - echo "🗺  Listing files in repo root for sanity check"
-      - pwd
-      - ls -alh
+      - ls -R .
       - echo "🔄  Syncing repo → S3 (deleting any stale objects)…"
-      - aws s3 sync . s3://$TARGET_BUCKET --delete --exclude "Terraform/*" --exclude "Terraform" --exclude ".git/*" --exclude ".git" --exclude "README.md" --exclude ".gitignore" --exclude ".gitattributes"
+      - |
+        aws s3 sync . s3://$TARGET_BUCKET \
+          --delete \
+          --exclude "Terraform/*" \
+          --exclude ".git/*"
 BUILD_SPEC
-    )
-  }
-
-  logs_config {
-    cloudwatch_logs {
-      group_name  = "/aws/codebuild/taskflow-frontend-sync"
-      stream_name = "sync-logs"
-    }
   }
 }
